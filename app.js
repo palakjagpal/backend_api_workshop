2d21da5ed3a1bf7eee35a02b768c8e74a3247eee
Palak Jagpal <palakjagpal70@gmail.com>
2026-04-04 20:26:13 +0530

    added item module

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
 import connectDB from './config/db.js';
+import  itemRoutes from './routes/itemRoute.js';
+
 
 dotenv.config();
 
@@ -14,7 +16,10 @@ app.use(cors());
 
 connectDB();
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/items", itemRoutes);
+
+
 
 app.get("/",(req,res)=> {
     res.send("🚀 Auth API is running...")
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-
-import User from '../model/authmodel.js';
-
-dotenv.config();
-
- export const signup = async(req,res)=>
- {   
-    try
-    {
-        const{name,email,password}=req.body;
-        const exist=await User.findOne({email})
-       if (exist) {
-           return res.status(400).json({ msg: "User already registered" });
-}
-        const hashed=await bcrypt.hash(password,10);
-        const data= new User({name,email,password:hashed});
-        console.log("data is saved");
-        await data.save();
-        console.log("Saving user:", data);
-        // res.status(200).json({msg:"data saved" });
-        res.json({
-          msg: "data saved",
-         user: { name, email }
-            });
-    }
-
-  catch (error) {
-  console.error(error); // This will print the real error in your terminal
-  res.status(500).json({ msg: "cant signup", error: error.message });
-}
-
-}
-
-export const  login= async(req,res)=>
-    
-    {
-        try{
-      const{email,password}=req.body;
-      const user=await User.findOne({email});
-      if(!user) return res.status(401).json({msg:"not correct email"});
-      const valid =await bcrypt.compare(password,user.password)
-      if(!valid) return res.status(401).json({msg:"wrong password"});
-
-      const token= jwt.sign({id:user._id,email:user.email,name: user.name},process.env.JWT_SECRET,{expiresIn:"1h" });
-
-    res.status(200).json({
-            msg: "user login successfully",
-            token,
-            user: {
-                name: user.name,
-                email: user.email
-                      }
-                    });
-        }
-        catch(error)
-        {
-             res.status(400).json({msg:"cant login"});
-        }
-    }
-    
-
-
-
-
-=======
 import User_Model from "../models/authModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
@@ -160,4 +91,3 @@ export const publicRoute = (req,res) => {
     console.log("Accessing public route")
 
 }
->>>>>>> d36145242b138b23435ac5c73f9b664e2792fb0e
